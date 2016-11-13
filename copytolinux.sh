4e79a884493334c7e34cc8cdfdb28861630841f0
Huo <huoy1@masi-16.ds.vanderbilt.edu>
2016-11-13 09:48:42 -0600

    add

---
+++ copytolinux.sh
@@ -0,0 +1,5 @@
+#!bin/bash 
+
+cp antsJointFusion.cxx /fs4/masi/huoy1/MaCRUISEsurf/code/ANTs/ANTs/Examples/antsJointFusion.cxx
+cp itkWeightedVotingFusionImageFilter.h /fs4/masi/huoy1/MaCRUISEsurf/code/ANTs/ANTs/ImageSegmentation/itkWeightedVotingFusionImageFilter.h
+cp itkWeightedVotingFusionImageFilter.hxx /fs4/masi/huoy1/MaCRUISEsurf/code/ANTs/ANTs/ImageSegmentationitk/WeightedVotingFusionImageFilter.hxx
